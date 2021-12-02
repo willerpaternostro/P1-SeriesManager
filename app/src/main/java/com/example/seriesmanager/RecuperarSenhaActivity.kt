d292ece0beb44c2570a0f4d4ff0275f3317b2824
willerpaternostro <willergmpaternostro@gmail.com>
2021-12-02 00:14:07 -0300

    Recuperação de senha finalizado

--- app/src/main/java/com/example/seriesmanager/RecuperarSenhaActivity.kt
+++ app/src/main/java/com/example/seriesmanager/RecuperarSenhaActivity.kt
@@ -2,6 +2,7 @@ package com.example.seriesmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.seriesmanager.databinding.ActivityRecuperarSenhaBinding
 
 class RecuperarSenhaActivity : AppCompatActivity() {
@@ -12,5 +13,20 @@ class RecuperarSenhaActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(activityRecuperarSenhaBinding.root)
         supportActionBar?.subtitle = "Recuperar senha"
+        with(activityRecuperarSenhaBinding){
+            enviarEmailBt.setOnClickListener {
+                val email = editTextEmail.text.toString()
+                if(email.isNotEmpty()){
+                    AutenticacaoFirebase.firebaseAuth.sendPasswordResetEmail(email).addOnCompleteListener { envio ->
+                        if(envio.isSuccessful){
+                            Toast.makeText(this@RecuperarSenhaActivity, "E-mail de recuperação enviado", Toast.LENGTH_SHORT).show()
+                            finish()
+                        }else{
+                            Toast.makeText(this@RecuperarSenhaActivity, "Falha no envio de e-mail de recuperação", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
+        }
     }
 }
